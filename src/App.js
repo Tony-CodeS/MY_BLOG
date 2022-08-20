5fef817fe93ab20129c4eb701f84e26dd4591855
Tony <asolomon484@gmail.com>
2022-08-20 13:15:16 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Nav from './components/Nav';
+import TechNews from './pages/Tech-News';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import NEWSFROMUSA from './pages/News-From-USA';
+import BBC from './pages/BBC';
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path ="/" element ={<TechNews />}/>
+        <Route path ="/About" element ={<NEWSFROMUSA />}/>
+        <Route path ="/Contact" element ={<BBC />}/>
+      </Routes>
+      </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
